3eae60a004733b50e8b6bbe71933374e1561cbbc
Levhcwm21. <alexags1983@gmail.com>
2025-11-29 14:53:12 +0530

    Bugs fixed

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,7 +3,6 @@ from logging import INFO, ERROR, FileHandler, StreamHandler, basicConfig, getLog
 from traceback import format_exc
 from asyncio import Queue, Lock
 
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 from dotenv import load_dotenv
@@ -39,7 +38,6 @@ class Var:
         LOGS.critical('Important Variables Missing. Fill Up and Retry..!! Exiting Now...')
         exit(1)
 
-    # REMOVED: RSS_ITEMS completely — now fully schedule-based
     FSUB_CHATS = list(map(int, getenv('FSUB_CHATS', '').split())) if getenv('FSUB_CHATS') else []
     BACKUP_CHANNEL = getenv("BACKUP_CHANNEL") or ""
     MAIN_CHANNEL = int(getenv("MAIN_CHANNEL"))
@@ -48,7 +46,7 @@ class Var:
     ADMINS = list(map(int, getenv("ADMINS", "1242011540").split()))
     
     SEND_SCHEDULE = getenv("SEND_SCHEDULE", "False").lower() == "true"
-    BRAND_UNAME = getenv("BRAND_UNAME", "@Animes_Guy")
+    BRAND_UNAME = getenv("BRAND_UNAME", "@username")
     FFCODE_1080 = getenv("FFCODE_1080") or """ffmpeg -i '{}' -progress '{}' -preset veryfast -c:v libx264 -s 1920x1080 -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 '{}' -y"""
     FFCODE_720 = getenv("FFCODE_720") or """ffmpeg -i '{}' -progress '{}' -preset superfast -c:v libx264 -s 1280x720 -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 '{}' -y"""
     FFCODE_480 = getenv("FFCODE_480") or """ffmpeg -i '{}' -progress '{}' -preset superfast -c:v libx264 -s 854x480 -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 '{}' -y"""
