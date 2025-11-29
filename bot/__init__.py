473fe5f6566725c7ecc6d1ae909ff0ed27a60bd9
Levhcwm21. <alexags1983@gmail.com>
2025-11-29 12:39:54 +0530

    Unwanted update

--- bot/__init__.py
+++ bot/__init__.py
@@ -76,7 +76,7 @@ if not ospath.isdir("downloads/"):
 try:
     bot = Client(name="AutoAniAdvance", api_id=Var.API_ID, api_hash=Var.API_HASH, bot_token=Var.BOT_TOKEN, plugins=dict(root="bot/modules"), parse_mode=ParseMode.HTML)
     bot_loop = bot.loop
-    sch = AsyncIOScheduler(timezone="Asia/Kolkata")
+    sch = AsyncIOScheduler(timezone="Asia/Kolkata", event_loop=bot_loop)
 except Exception as ee:
     LOGS.error(str(ee))
     exit(1)
