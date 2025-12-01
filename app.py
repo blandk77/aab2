0629bac51fccabff2c4043ba7a75e4d718095f8a
Levhcwm21. <alexags1983@gmail.com>
2025-12-01 14:59:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from aiohttp import web
+from flask import Flask
+web = Flask(__name__)
 
-async def ping(request):
-    return web.Response(text="OK")
+@web.route('/')
+def hello_world():
+    return 'Status: Active...'
 
-def web_server():
-    app = web.Application()
-    app.add_routes([web.get("/", ping)])
-    web.run_app(app, host="0.0.0.0", port=7860)
+
+if __name__ == "__main__":
+    web.run()
