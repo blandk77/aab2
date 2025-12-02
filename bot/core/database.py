aed31c2bdd8094f03909c10472c9518e8fee8df1
Levhcwm21. <alexags1983@gmail.com>
2025-12-02 20:33:28 +0530

    Bugs fixed

--- bot/core/database.py
+++ bot/core/database.py
@@ -34,9 +34,9 @@ class MongoDB:
                     {'_id': ani_id},
                     {'$unset': {old_ep: ""}}
                 )
-
+  
     # === NEW SCHEDULE SYSTEM ===
-    async def saveSchedule(self, name, rss_links, platform, audio_pref, custom_title, timestamp):
+    async def saveSchedule(self, name, rss_links, platform, audio_pref, custom_title, timestamp, file_path=None, episode_num=None, ani_data=None):
         doc = {
             '_id': str(hash(name + ''.join(rss_links))),  # unique
             'name': name,
